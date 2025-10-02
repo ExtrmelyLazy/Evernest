5916115001ae2e9580d76655e03075178ec857f8
ExtrmelyLazy <Victoria05161@outlook.com>
2025-10-02 13:11:17 +0800

    Initial commit: VitePress project setup

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,10 @@
+export default {
+  title: '我的网站',
+  description: '运行在 Termux 上的 VitePress 网站',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '关于', link: '/about' }
+    ]
+  }
+}
---
+++ docs/index.md
@@ -0,0 +1,7 @@
+# Hello VitePress
+
+欢迎来到我的 VitePress 网站！
+
+- 快速
+- 简单  
+- 强大
