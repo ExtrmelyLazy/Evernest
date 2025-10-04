2a6fcb613dd5a9b7682990436c34540ae9428882
ExtrmelyLazy <victoria05161@outlook.com>
2025-10-04 22:38:28 +0800

    Update index.js

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,9 @@
+// .vitepress/theme/index.js
 import DefaultTheme from 'vitepress/theme'
 import './index.css'
+import CustomLayout from './Layout.vue'
 
 export default {
-  extends: DefaultTheme
+  extends: DefaultTheme,
+  Layout: CustomLayout
 }
